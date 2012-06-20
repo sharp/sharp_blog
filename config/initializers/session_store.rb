e25327d10853aca466bfe5e28c18e90abad04e3d
sharp <sharp@sharp-Rev-1-0.(none)>
2012-06-20 09:17:21 +0800

    upgrade rails to 3.2.6

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,5 @@
 # Be sure to restart your server when you modify this file.
-
-SharpBlog::Application.config.session_store :cookie_store, key: '_sharp_blog_session'
+SharpBlog::Application.config.session_store :cookie_store, :key => '_sharp_blog_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
